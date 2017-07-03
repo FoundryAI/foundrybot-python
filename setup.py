138be78140e903704a628e8bc0b6a50dc5d2874b
Nick Gerner <njgerner@gmail.com>
2017-07-03 10:57:20 -0400

    update setup

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'Foundrybot python bindings',
   author = 'Nick Gerner',
   author_email = 'nick@foundry.ai',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  url = 'https://github.com/FoundryAI/foundrybot-python', # use the URL to the github repo
+  download_url = 'https://github.com/FoundryAI/foundrybot-python/archive/1.0.tar.gz',
+  keywords = ['foundrybot', 'foundry.ai', 'foundrydc'], # arbitrary keywords
   classifiers = [],
 )
