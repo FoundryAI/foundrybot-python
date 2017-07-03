91aaf0b530f1037cd5618f2304e061cfe4695ab3
Nick Gerner <njgerner@gmail.com>
2017-07-03 10:48:24 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'foundrybot',
+  packages = ['foundrybot'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Foundrybot python bindings',
+  author = 'Nick Gerner',
+  author_email = 'nick@foundry.ai',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
